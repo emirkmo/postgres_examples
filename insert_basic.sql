d8a87cf11e6b0efb973a06bf6bc4c346a30d5a66
Emir Karamehmetoglu <emir.k@phys.au.dk>
2022-11-06 02:37:14 +0100

    add basic insert, test #1

---
+++ insert_basic.sql
@@ -0,0 +1,1 @@
+INSERT INTO projects(name,repo,link,tagline) VALUES ('flows','https://github.com/SNFLOWS/flows','https://flows.phys.au.dk','AARHUS-BARCELONA FLOWS PIPELINE. OPEN SOURCE SUPERNOVA (PSF) PHOTOMETRY PIPELINE WITH 30+ INSTRUMENTS');
